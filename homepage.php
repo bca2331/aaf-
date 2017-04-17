56bc7d338db6a07403dc879eb02875f89ca6a369
bca2331 <bca2331@gmail.com>
2017-04-16 21:49:26 -0400

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,97 @@
+<!--Authors: Briana Arrington, Dyla Gunn, Ningxin Shi
+    Date: 4/16/17
+    Class: 710 Spec & Design -->
+
+
+
+
+<?php 
+// Start the session, this needs to be on every page
+session_start();
+?><!DOCTYPE html>
+<html>
+<head>
+
+<link rel="stylesheet" href="FacultyStyle.css">
+<style>
+    
+body {//background-color:lightgrey;}
+h1   {color:white;
+      text-align:center;}
+p    {color:white;}  
+    
+ 
+    
+        
+ul {
+	list-style-type: none;
+	margin: 0;
+	overflow: hidden;
+	background-color: #373EC1;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+
+li a:hover {
+    background-color: #B8B7B7;
+}       
+    </style>
+    </head>
+    <body>
+    
+    <a href= "../index.php" target="_blank" >
+    <center><img src="aaf.png" alt="AA&F logo" style="width:304px;height:228px;"></center>
+    </a>
+        <h1 style="color:Black;"><center>Welcome to the Aviation Arms & Flight Information System!</center></h1>
+        
+    <ul>
+        
+        <li><a href="year1.php">Mission Statement</a></li>
+        <li><a href="year2.php">Lookup Contract</a></li>
+        <li><a href="year3.php">Part Catalog</a></li>
+        <li><a href="year5.php">Request Meeting</a></li>
+        
+        <li><a href="../index.php">Sign Out</a></li>
+        
+        
+        </ul>
+    
+  <p> AA & F Homepage </p>
+        
+        <center><img src="propeller.jpg" alt="AA&F Logo" style="width:500px;height:400px;"></center>
+        
+        
+    
+   
+
+    </body>
+    </html>
+        
+   
+
+    </body>
+    
+    <br><br><br>
+     Please Follow Us @AA&F!:
+    <script type="text/javascript" src="https://ws.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "389b443c-450b-475f-9c48-37d9c88e353d", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
+
+
+<span class='st_facebook_large' displayText='Facebook'></span>
+<span class='st_twitter_large' displayText='Tweet'></span>
+<span class='st_linkedin_large' displayText='LinkedIn'></span>
+<span class='st_pinterest_large' displayText='Pinterest'></span>
+<span class='st_google_large' displayText='Google'></span>
+</html>
+</html>
